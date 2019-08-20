8b06a454c41f7ff56097983b22d83622003fc879
charlescortez <charlesmph@gmail.com>
2019-08-20 16:14:28 -0400

    added to router.post to check for valid username and valid password that matches the stored hashed password

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.post('/registerProcess', function(req, res, next) {
   function insertUser(){
   const insertUserQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) returning id`
 
-const hash = bcrypt.hashSync(password, 10);
+  const hash = bcrypt.hashSync(password, 10);
 
   db.one(insertUserQuery, [username, email, hash]).then((resp)=>{
     res.json({
@@ -36,5 +36,35 @@ const hash = bcrypt.hashSync(password, 10);
   })
 }
 })
+
+router.post('/loginProcess', (req, res, next)=>{
+  // res.json(req.body)
+
+  const checkUserQuery = `SELECT * FROM users WHERE username=$1`;
+
+  const checkUser = db.one(checkUserQuery, [req.body.username])
+
+  checkUser.then((results)=>{
+    // user exists. check the pass
+    const correctPass = bcrypt.compareSync(req.body.password, results.password);
+    if(correctPass) {
+      // this is a valid user/pass
+      res.json("Logged in")
+    } else {
+      // these aren't the droids were looking for
+      res.redirect('/login?msg=badPass')
+    }
+    res.json(results);
+  })
+
+  checkUser.catch((error)=>{
+    res.json({
+      msg: "userDoesNotExist"
+    })
+  })
+
+
+})
+
 
 module.exports = router;
