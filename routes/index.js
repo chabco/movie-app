7440a7a2b10bccac41bf1d187873f551ed23fb56
charlescortez <charlesmph@gmail.com>
2019-08-20 14:22:04 -0400

    added working register page that inserts body into db

--- routes/index.js
+++ routes/index.js
@@ -40,4 +40,10 @@ router.get('/movie/:id', (req, res, next)=> {
   });
 });
 
+router.get('/register', (req, res, next)=>{
+  res.render('register', {
+
+  });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/registerProcess', function(req, res, next) {
+
+  const { username, email, password, passowrd2 } = req.body;
+
+  const insertUserQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) returning id`
+  db.one(insertUserQuery, [username, email, password]).then((resp)=>{
+    res.json({
+      msg: 'useradded'
+    })
+
+  })
+
 });
 
 module.exports = router;
